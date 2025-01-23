58d4d1c5830dbd6c8fea449a5a112041890beee2
whymami <mmg165316@gmail.com>
2025-01-24 01:37:04 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,14 @@ done
 echo -e "\033[0;32m/var/lib/tor/hidden_service/hostname file found!\033[0m"
 
 chown -R torUser:torUser /etc/tor /var/lib/tor /var/log/tor
-echo -e "\033[0;32mowner changed!\033[0m"
+echo -e "\033[0;32mOwner changed!\033[0m"
 sleep 2
 
+rm -rf /home/torUser/entrypoint.sh
+echo -e "\033[0;32mEntrypoint.sh deleted!\033[0m"
+
+echo -e "ONION URL: "
+echo -e "\033[1m\033[0;33m$(cat /var/lib/tor/hidden_service/hostname)\033[0m"
+
+# Ensure nginx runs in the foreground, allowing terminal outputs to continue
 nginx -g 'daemon off;'
